e376d03c5ae7683bc7c14ee90173ecfcdab5c76e
Richie Preece <richie.preece@gmail.com>
2013-07-06 20:35:06 -0600

    Breaking into multiple files

---
+++ tools/tools.js
@@ -0,0 +1,82 @@
+var shared   = require('../shared.js')
+	, fs       = require('fs')
+	, url      = require('url')
+	, https    = require('https')
+	;
+
+exports.getVersions = getVersions;
+exports.versions = versions;
+
+/**
+ * This method gets the versions.json file from the mojang servers
+ */
+function getVersions(){
+	//Location of the versions.json file
+	var mojangVersionUrl = "https://s3.amazonaws.com/Minecraft.Download/versions/versions.json";
+	
+	var options = {
+		host: url.parse(mojangVersionUrl).host,
+		port: 443,
+		path: url.parse(mojangVersionUrl).path
+	}
+	
+	//Get the file
+	var request = https.request(options, function(result){		
+		if(result.statusCode == 200){		
+			result.on('data', function(data){
+				var currDir = process.cwd();
+			
+				//Ensure the versions folder exists
+				if(!fs.existsSync("versions")){
+					fs.mkdirSync("versions");
+				}
+				
+				process.chdir("versions");
+				
+				//Write out to a file for safe keeping
+				var file = fs.createWriteStream(process.cwd() + '/versions.json');				
+				file.write(data);				
+				file.end();
+				
+				process.chdir(currDir);				
+				
+				//Set the versions data for use (this way we have no IO when versions are requested)
+				shared.set('versions', JSON.parse(data));
+			});
+		}
+	});
+	
+	request.end();
+}
+
+/**
+ * This method returns a list of versions
+ */
+function versions(request, response, next){
+	var responseData = {};
+	
+	//Check for a logged in user
+	if(request.session.user){
+		var isAllowed = false;
+		
+		//Check permissions
+		for(index in request.session.user['acl']){
+			if(request.session.user['acl'][index] == 'GET_VERSIONS'){
+				isAllowed = true;
+			}
+		}
+	
+		if(isAllowed){
+			//Set the list of versions
+			responseData['versions'] = shared.get('versions');
+		} else {
+			responseData['sucess'] = false;
+			responseData['err'] = 'You do not have the necessary permissions';
+		}
+	} else {
+		responseData['success'] = false;
+		responseData['err'] = 'You are not logged in';
+	}
+	
+	response.send(responseData);
+}
