a512afc27b74e0854e67ce2fd8c476aaf627e249
numso <dosmun@gmail.com>
2013-07-09 21:32:45 -0600

    worked on users page

--- client/js/app/ctrl/users.js
+++ client/js/app/ctrl/users.js
@@ -1,5 +1,37 @@
 angular.module('minetrocity').controller('usersCtrl',
-  function ($scope, usersData) {
-    $scope.users = usersData.getUsers();
+  function ($scope, $http, usersData) {
+    usersData.getData().then(
+      function (resp) {
+        $scope.users = resp;
+      },
+      function (err) {
+        console.log(err);
+      }
+    );
+
+    $scope.updateUser = function (user) {
+
+      var newUser = {
+        id: user.id,
+        username: user.username,
+        email: user.email,
+        acl: []
+      };
+
+      for (var i = 0; i < user.acl.length; ++i) {
+        if (user.acl[i].has) {
+          newUser.acl.push(user.acl[i].name);
+        }
+      }
+
+      $http.put('/update_user', newUser).then(
+        function (resp) {
+          console.log('updated successfully!!');
+        },
+        function (err) {
+          console.log('could not update');
+        }
+      );
+    };
   }
 );
--- client/js/app/service/usersData.js
+++ client/js/app/service/usersData.js
@@ -1,5 +1,16 @@
 angular.module('minetrocity').factory('usersData',
   function ($http, $q) {
+    function getPermissions() {
+      var deferred = $q.defer();
+      $http.get('/permissions').then(
+        function (resp) {
+          deferred.resolve(resp.data.permissions.permissions);
+        },
+        deferred.reject
+      );
+      return deferred.promise;
+    }
+
     function getUsers() {
       var deferred = $q.defer();
       $http.get('/users').then(
@@ -19,8 +30,41 @@ angular.module('minetrocity').factory('usersData',
       return deferred.promise;
     }
 
+    function getData() {
+      var deferred = $q.defer();
+      $q.all([
+        getUsers(),
+        getPermissions()
+      ]).then(
+        function (resp) {
+          var users = resp[0];
+          var perms = resp[1];
+          for (var i = 0; i < users.length; ++i) {
+            var acl = users[i].acl;
+            users[i].acl = [];
+            for (var j = 0; j < perms.length; ++j) {
+              users[i].acl.push({ name: perms[j], has: contains(perms[j], acl) });
+            }
+          }
+          deferred.resolve(users);
+        },
+        deferred.reject
+      );
+      return deferred.promise;
+    }
+
+
+    function contains(perm, acl) {
+      for (var i = 0; i < acl.length; ++i) {
+        if (acl[i] === perm) {
+          return true;
+        }
+      }
+      return false;
+    }
+
     return {
-      getUsers: getUsers
+      getData: getData
     };
   }
 );
