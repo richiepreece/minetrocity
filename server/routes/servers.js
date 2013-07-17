13404af096b71746906b5db37a028bf7daa39af7
Richie Preece <richie.preece@gmail.com>
2013-07-16 22:22:34 -0600

    I believe the server methods are updated. Probably have bugs, but I can't test it yet.

--- server/routes/servers.js
+++ server/routes/servers.js
@@ -362,101 +362,56 @@ function commandServer(request, response, next){
   });
 }
 
-/************************************************************************************************************************************************/
-
-/**
- * This method updates a server
- */
 function updateServer(request, response, next){
-  var responseData = {};
-
-  //Check for a logged in user
-  if(request.session.user){
-    var isAllowed = false;
-
-    //Check permissions
-    for(index in request.session.user['acl']){
-      if(request.session.user['acl'][index] == 'UPDATE_SERVERS'){
-        isAllowed = true;
-      }
-    }
-
-    if(isAllowed){
-      //Get updated data and the existing server
-      var updatedServer = request.body;
-      var oldServer = shared.get('servers')[updatedServer['id']];
-
-      if(oldServer){
-        //Make sure the server isn't running before we update
-        if(!shared.get('child' + oldServer['id'])){
-          //Grab old data that will need to be used
-          var oldFsSafeName = oldServer['server_name'].replace(/\W/g, '');
-          var oldName = oldServer['server_name'];
-          var oldPort = oldServer['port'];
-
-          //Update with specified data
-          for(index in updatedServer){
-            oldServer[index] = updatedServer[index];
-          }
-
-          //Grab new data that will need to be used
-          var newFsSafeName = oldServer['server_name'].replace(/\W/g, '');
-          var newName = oldServer['server_name'];
-          var newPort = oldServer['port'];
-
-          //Output all servers
-          fs.writeFileSync('models/servers.json', JSON.stringify(shared.get('servers')));
-
-          var currDir = process.cwd();
-
-          //Navigate to the worlds folder
-          if(fs.existsSync('worlds')){
-            process.chdir('worlds');
-
-            //Navigate to the server-specific folder
-            if(fs.existsSync(oldServer['id'])){
-              process.chdir(oldServer['id']);
-
-              //If the world-name folder exists, rename it
-              if(fs.existsSync(oldFsSafeName)){
-                fs.renameSync(oldFsSafeName, newFsSafeName);
-              }
-
-              //Open the server.properties, and make sure all data is accurate
-              var serverProps = fs.readFileSync('server.properties', { encoding : 'utf8' });
-              var serverProps = serverProps.replace(oldFsSafeName, newFsSafeName);
-              var serverProps = serverProps.replace(oldName, newName);
-              var serverProps = serverProps.replace('server-port=' + oldPort, 'server-port=' + newPort);
-
-              fs.writeFileSync('server.properties', serverProps);
-            }
-          }
-
-          process.chdir(currDir);
-
-          //Make sure we have the correct minecraft_server.jar file
-          getServer(oldServer);
-
-          responseData['id'] = updatedServer['id'];
-          responseData['success'] = true;
-        } else {
-          responseData['success'] = false;
-          responseData['err'] = "Server is running. Can't update.";
-        }
-      } else {
-        responseData['success'] = false;
-        responseData['err'] = 'User does not exist';
-      }
-    } else {
-      responseData['success'] = false;
-      responseData['err'] = 'You do not have the necessary permissions';
-    }
-  } else {
-    responseData['success'] = false;
-    responseData['err'] = 'You are not logged in';
+  function fail(reason){
+    response.send({
+      success : false,
+      err : reason
+    });
   }
 
-  response.send(responseData);
+  if(request.session.user === undefined) return fail('You are not logged in');
+
+  var allowed = false;
+
+  //Check permissions
+  for(index in request.session.user['acl']){
+    if(request.session.user['acl'][index] == 'UPDATE_SERVERS'){
+      isAllowed = true;
+    }
+  }
+
+  if(!isAllowed) return fail('You do not have the necessary permissions');
+
+  var server = request.body;
+
+  if(server.id === undefined) return fail('You must specify a server id');
+  if(shared.get('servers')[server.id] === undefined) return fail('The server doesn\'t exist');
+  if(shared.get('child' + server.id) !== undefined) return fail('The server is running');
+
+  var oldServer = shared.get('servers')[server.id];
+
+  for(index in server.mainproperties){
+    oldServer.mainproperties[index] = server.mainproperties[index];
+  }
+
+  for(index in server.properties){
+    oldServer.properties[index] = server.properties[index];
+  }
+
+  for(index in server.udfproperties){
+    oldServer.udfproperties[index] = server.udfproperties[index];
+  }
+
+  fs.writeFileSync('models/servers.json', JSON.stringify(shared.get('servers'), null, '\t'));
+
+  createWorld(oldServer);
+  getServer(oldServer);
+
+  response.send({
+    id : oldServer.id,
+    success : true
+  });
 }
 
 /***************************
